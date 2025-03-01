3efa0583897305edde0e311435e2f6f16098c542
WonJin <try3982@kakao.com>
2025-03-01 17:33:58 +0900

    1주차

---
+++ src/main/java/weeks01/Variables.java
@@ -0,0 +1,18 @@
+package weeks01;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age;
+        age = 27;
+
+        int num =21;
+        age = 26;
+
+        age = num; //21
+        age = age+1; //22
+
+        System.out.print("Hello, I'm ");
+        System.out.print(age);
+        System.out.println(".");
+    }
+}
